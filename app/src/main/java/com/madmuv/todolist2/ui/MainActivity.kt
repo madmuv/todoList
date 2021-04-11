29e4e4e6db000dd391072635348f9d3f996a1112
Donyawan Kritaisong <donyawan.kri@cpmatch.com>
2021-04-11 19:30:37 +0700

    add popup

--- app/src/main/java/com/madmuv/todolist2/ui/MainActivity.kt
+++ app/src/main/java/com/madmuv/todolist2/ui/MainActivity.kt
@@ -2,11 +2,20 @@ package com.madmuv.todolist2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.madmuv.todolist2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
